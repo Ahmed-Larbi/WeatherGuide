f9c54ec7a2b8623d39f1aecda8a6e35b17a7c98d
ahmedlarbi <hamooda2002011@hotmail.com>
2022-10-07 22:29:08 +0400

    Added Search Bar with Functionality

--- weatherapp/lib/main.dart
+++ weatherapp/lib/main.dart
@@ -29,9 +29,10 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   //Testing Api Call
-  String location = "dubai";
+  String location = "Abu Dhabi";
   WeatherApiClient client = WeatherApiClient();
   Weather? data;
+  final searchBarController = TextEditingController();
 
   Future<void> getData() async {
     data = await client.getCurrentWeather(location);
@@ -48,54 +49,80 @@ class _HomePageState extends State<HomePage> {
         appBar: AppBar(
           backgroundColor: Colors.blue[100],
           elevation: 0,
-          title: Text(
+          title: const Text(
             "WEATHERAPP",
             style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
           ),
           centerTitle: true,
           leading: IconButton(
             onPressed: () {},
-            icon: Icon(Icons.menu),
+            icon: const Icon(Icons.menu),
             color: Colors.amber,
           ),
         ),
-        body: FutureBuilder(
-          future: getData(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  currentWeather(Icons.wb_sunny_rounded, "${data!.temp}",
-                      "${data!.cityName}"),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    "Today's Highlights",
-                    style: TextStyle(
-                        fontSize: 24.0,
-                        color: Color(0xdd212121),
-                        fontWeight: FontWeight.bold),
-                  ),
-                  const Divider(
-                    height: 20,
-                    thickness: 3,
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  additionalInfo("${data!.wind} kp_h", "${data!.pressure}",
-                      "${data!.feel}c", "${data!.humidity}%")
-                ],
-              );
-            } else if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            return Container();
-          },
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              controller: searchBarController,
+              textAlign: TextAlign.center,
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(width: 3, color: Colors.white),
+                    borderRadius: BorderRadius.circular(50)),
+                hintText: 'Enter city Name',
+              ),
+              onSubmitted: (value) {
+                if (searchBarController.text.isNotEmpty) {
+                  location = searchBarController.text;
+                  setState(() {});
+                  searchBarController.clear();
+                }
+              },
+            ),
+            FutureBuilder(
+              future: getData(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      currentWeather(Icons.wb_sunny_rounded, "${data!.temp}",
+                          "${data!.cityName}"),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      const Text(
+                        "Today's Highlights",
+                        style: TextStyle(
+                            fontSize: 24.0,
+                            color: Color(0xdd212121),
+                            fontWeight: FontWeight.bold),
+                      ),
+                      const Divider(
+                        height: 20,
+                        thickness: 3,
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      additionalInfo("${data!.wind} kp_h", "${data!.pressure}",
+                          "${data!.feel}c", "${data!.humidity}%")
+                    ],
+                  );
+                } else if (snapshot.connectionState ==
+                    ConnectionState.waiting) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                return Container();
+              },
+            ),
+          ],
         ));
   }
 }
